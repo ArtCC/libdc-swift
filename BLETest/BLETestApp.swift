2daa6d551b22403f85a9dc04d474e1563e15a791
latishab <latishabesariani@gmail.com>
2024-07-04 20:04:04 +0800

    Initial Commit

---
+++ BLETest/BLETestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BLETestApp.swift
+//  BLETest
+//
+//  Created by Latisha Besariani on 04/07/24.
+//
+
+import SwiftUI
+
+@main
+struct BLETestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
