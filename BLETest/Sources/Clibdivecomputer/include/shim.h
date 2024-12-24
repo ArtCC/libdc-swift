8233297cf8ce301b772b83b475c6fcec30fdf118
Latisha <latishabesariani@gmail.com>
2024-12-24 20:45:54 +0800

    Switch libdivecomputer to git submodule

---
+++ BLETest/Sources/Clibdivecomputer/include/shim.h
@@ -0,0 +1,19 @@
+//
+//  shim.h
+//  BLETest
+//
+//  Created by User on 24/12/2024.
+//
+
+#ifndef LIBDC_SHIM_H
+#define LIBDC_SHIM_H
+
+#include <libdivecomputer/common.h>
+#include <libdivecomputer/context.h>
+#include <libdivecomputer/descriptor.h>
+#include <libdivecomputer/device.h>
+#include <libdivecomputer/parser.h>
+#include <libdivecomputer/iostream.h>
+#include <libdivecomputer/custom.h>
+
+#endif /* LIBDC_SHIM_H */
---
+++ BLETest/scripts/build-xcframework.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+#  build-xcframework.sh
+#  BLETest
+#
+#  Created by User on 24/12/2024.
+#  
